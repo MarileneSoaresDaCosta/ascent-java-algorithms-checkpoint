2fb41e6ae4a59aa8c36ada57ec54b101e17bac39
Marilene Soares da Costa <lene626@gmail.com>
2022-07-18 16:17:28 -0600

    adds allEqual method

--- src/main/java/com/galvanize/Algorithm.java
+++ src/main/java/com/galvanize/Algorithm.java
@@ -3,4 +3,30 @@ package com.galvanize;
 
 public class Algorithm {
 
+    public boolean allEqual(String word) {
+        // accumulator 
+        boolean result = true;
+        
+        // empty case
+        if(word.length() == 0 ) {
+            return false;
+        }
+
+        // only for strings with more than 1 char
+        if(word.length() == 1) {
+            return result;
+        }
+
+
+        // get first char, to compare
+        char firstChar = Character.toLowerCase(word.charAt(0));
+
+        // first char
+        for (int i = 1; i < word.length() ; i++) {
+            if(Character.toLowerCase(word.charAt(i)) != firstChar) {
+                return false;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/galvanize/AlgorithmTest.java
+++ src/test/java/com/galvanize/AlgorithmTest.java
@@ -6,6 +6,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlgorithmTest {
+    @Test
+    public void returnsTrueForAllAs() {
+        //setup
+        Algorithm algorithm = new Algorithm();
+        String allAs = "aAa";
+        //execution
+        boolean expected = true;
+        boolean actual = algorithm.allEqual(allAs);
+        //assertion
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void returnFalseForAsAndBs() {
+//        // set up
+        Algorithm algorithm = new Algorithm();
+        String asAndBs = "bbBbabbb";
+//        //execution
+        boolean expected = false;
+        boolean actual = algorithm.allEqual(asAndBs);
+//        //assertion
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void returnFalseForEmptyString() {
+//        // set up
+        Algorithm algorithm = new Algorithm();
+        String empty = "";
+//        //execution
+        boolean expected = false;
+        boolean actual = algorithm.allEqual(empty);
+//        //assertion
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void returnTrueForSingleChar() {
+//        // set up
+        Algorithm algorithm = new Algorithm();
+        String single = "a";
+//        //execution
+        boolean expected = true;
+        boolean actual = algorithm.allEqual(single);
+//        //assertion
+        assertEquals(expected, actual);
+    }
 
 
 }
